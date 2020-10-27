fea5b9f6f82254ac3a80435682377dba958b5f10
lezizijiang <lizhangzijian@126.com>
2020-10-27 15:24:45 +0800

    作业完成

--- src/Vertex/Graph.java
+++ src/Vertex/Graph.java
@@ -1,6 +1,5 @@
 package Vertex;
 
-import javax.print.DocFlavor;
 import java.io.*;
 import java.util.*;
 
@@ -9,12 +8,14 @@ import java.util.*;
  */
 public class Graph{
     int maxVertex = 0;
+    int curLabel, numScc;
+    int[] marked;
+    PriorityQueue<Integer> priorityQueue;
+    int[] f;
+    final int MARKED = 1;
+    final int UNMARKED = 0;
     HashMap<Integer, List<Integer>> map;
     HashMap<Integer, List<Integer>> reverseMap;
-    public Graph(){
-        this.map = new HashMap<>();
-        this.reverseMap = new HashMap<>();
-    }
     public void readFile(String filename) throws IOException {
         // 利用BuffedReader读取文件
         BufferedReader file = new BufferedReader(new FileReader(filename));
@@ -22,6 +23,8 @@ public class Graph{
         int number, target;
         String[] temp1;
         List<Integer> temp2;
+        map = new HashMap<>();
+        reverseMap = new HashMap<>();
         // 将文件中的数据写入列表中
         while(str != null) {
             temp1 = str.split(" ");
@@ -45,15 +48,72 @@ public class Graph{
             str = file.readLine();
         }
     }
+
     public int[] kosaraju(){
-
+        marked = new int[maxVertex + 1];
+        f = new int[maxVertex + 1];
+        int[] res = new int[5];
+        for(int number : reverseMap.keySet()){
+            marked[number] = UNMARKED;
+            topoSort(reverseMap);
+        }
+        Comparator<Integer> comparator = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1-o2;
+            }
+        };
+        priorityQueue = new PriorityQueue<>(comparator);
+        for(int number : map.keySet()){
+            marked[number] = UNMARKED;
+        }
+        numScc = 0;
+        for (int i = 1; i < maxVertex + 1; i++) {
+            int vertex = f[i];
+            if (marked[vertex] == UNMARKED) {
+                numScc++;
+                dfs_scc(map, vertex);
+            }
+        }
+        for(int i = 0; i < 5; i++){
+            if(!priorityQueue.isEmpty()){
+                res[i] = priorityQueue.poll();
+            }else{
+                res[i] = 0;
+            }
+        }
+        return res;
     }
-    public void dfs_scc(int s){
-
+    public void dfs_scc(HashMap<Integer, List<Integer>>map,int vertex){
+        marked[vertex] = MARKED;
+        priorityQueue.add(numScc);
+        for(int v : map.get(vertex)){
+            if(marked[v] == MARKED)
+                dfs_scc(map, v);
+        }
     }
-    public void dfs_topo(int s)
-    public void topoSort(){
-        int[] marked = new int[maxVertex];
-
+    public void topoSort(HashMap<Integer, List<Integer>> map){
+        // for each方法
+        curLabel = maxVertex;
+        for (int vertex:map.keySet()) {
+            dfs_topo(map, vertex);
+        }
+        // Iterator 方法
     }
+    public void dfs_topo(HashMap<Integer, List<Integer>> map, int s){
+        marked[s] = MARKED;
+        for(int v : map.get(s)){
+            if(marked[v] == UNMARKED){
+                dfs_topo(map, v);
+            }
+        }
+        f[curLabel] = s;
+        curLabel--;
+    }
+    public static void main(String[] args) throws IOException {
+        Graph graph = new Graph();
+        graph.readFile("homework3/h3test1.txt");
+        System.out.println(Arrays.toString(graph.kosaraju()));
+    }
+
 }
