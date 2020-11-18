0efbdb9c18c33e7800a3f7a7dc55a7581734c70e
lezizijiang <lizhangzijian@126.com>
2020-11-18 14:41:09 +0800

    作业完成

---
+++ src/GreedyWork/UnionFind.java
@@ -0,0 +1,38 @@
+package GreedyWork;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 并查集定义
+ */
+public class UnionFind {
+
+    public static class UFNode {
+        //父节点
+        UFNode parent;
+    }
+
+    //查询
+    public static UFNode find(UFNode x) {
+        UFNode p = x;
+        //用于先把不同深度的节点存起来，最后统一指向根结点，加快查询效率
+        Set<UFNode> path = new HashSet<>();
+        while (p.parent != null) {
+            path.add(p);
+            p = p.parent;
+        }
+        //将所有边指向同一个集合标识（根节点）
+        for (UFNode ppp : path) {
+            ppp.parent = p;
+        }
+        //返回集合标识（根节点）
+        return p;
+    }
+
+    //合并
+    public static void union(UFNode x, UFNode y) {
+        //将两个不同的集合指向同一个集合标识（根节点）
+        find(y).parent = find(x);
+    }
+}
